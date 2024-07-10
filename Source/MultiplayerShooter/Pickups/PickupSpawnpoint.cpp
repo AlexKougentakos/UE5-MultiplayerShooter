81fd7fc280b6f0ab28ae686487258c423ab7fc39
Alex Kougentakos <77458838+AlexKougentakos@users.noreply.github.com>
2024-07-10 13:18:23 +0300

    Added pickup spawners
    These can be used for everything that inherits from pickup, that means ammo & powerups and you can decide what you want each pickup spawner to spawn.

---
+++ Source/MultiplayerShooter/Pickups/PickupSpawnpoint.cpp
@@ -0,0 +1,54 @@
+﻿#include "PickupSpawnpoint.h"
+
+#include "Pickup.h"
+
+APickupSpawnpoint::APickupSpawnpoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+}
+
+void APickupSpawnpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	StartSpawnTimer();
+}
+
+void APickupSpawnpoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickupSpawnpoint::SpawnPickup()
+{
+	checkf(m_PickupClasses.Num() > 0, TEXT("No pickup classes set in %s"), *GetName());
+	const int randomIndex = FMath::RandRange(0, m_PickupClasses.Num() - 1);
+	const TSubclassOf<APickup> pickupClass = m_PickupClasses[randomIndex];
+	if (!pickupClass) return;
+	
+	m_pCurrentPickup = GetWorld()->SpawnActor<APickup>(pickupClass, GetActorTransform());
+	if (!m_pCurrentPickup) return;
+
+	if (HasAuthority())
+		m_pCurrentPickup->OnDestroyed.AddDynamic(this, &APickupSpawnpoint::PickupDestroyed);
+}
+
+void APickupSpawnpoint::SpawnPickupFinished()
+{
+	if (HasAuthority())
+	{
+		SpawnPickup();
+	}
+}
+
+void APickupSpawnpoint::StartSpawnTimer()
+{
+	const float spawnTime = FMath::RandRange(m_MinSpawnDelay, m_MaxSpawnDelay);
+	GetWorldTimerManager().SetTimer(m_SpawnTimerHandle, this, &APickupSpawnpoint::SpawnPickupFinished, spawnTime, false);
+}
+
+void APickupSpawnpoint::PickupDestroyed(AActor* destroyedActor)
+{
+	StartSpawnTimer();
+}
+
---
+++ Source/MultiplayerShooter/Pickups/PickupSpawnpoint.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupSpawnpoint.generated.h"
+
+class APickup;
+
+UCLASS()
+class MULTIPLAYERSHOOTER_API APickupSpawnpoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APickupSpawnpoint();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Pickup", DisplayName = "Pickup Classes")
+	TArray<TSubclassOf<APickup>> m_PickupClasses{};
+
+	void SpawnPickup();
+	void SpawnPickupFinished();
+	void StartSpawnTimer();
+	UFUNCTION()
+	void PickupDestroyed(AActor* destroyedActor);
+
+	APickup* m_pCurrentPickup{nullptr};
+private:
+	FTimerHandle m_SpawnTimerHandle{};
+
+	UPROPERTY(EditAnywhere, Category = "Pickup", DisplayName = "Min Spawn Delay")
+	float m_MinSpawnDelay{ 5.f };
+	UPROPERTY(EditAnywhere, Category = "Pickup", DisplayName = "Max Spawn Delay")
+	float m_MaxSpawnDelay{ 10.f };
+	
+};

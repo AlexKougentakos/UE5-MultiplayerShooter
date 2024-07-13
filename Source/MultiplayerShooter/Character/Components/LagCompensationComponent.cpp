4304bcc71627a9937207b254a00b6028de77ed6c
Alex Kougentakos <77458838+AlexKougentakos@users.noreply.github.com>
2024-07-13 14:11:37 +0300

    Starting work on lag compensation component

---
+++ Source/MultiplayerShooter/Character/Components/LagCompensationComponent.cpp
@@ -0,0 +1,24 @@
+﻿#include "LagCompensationComponent.h"
+
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
---
+++ Source/MultiplayerShooter/Character/Components/LagCompensationComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LagCompensationComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MULTIPLAYERSHOOTER_API ULagCompensationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	ULagCompensationComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
